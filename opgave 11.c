ca2d9173becb19ce6b0cfc6b867bd854e2ab5130
RJmokaimonsteret <114211546+RJmokaimonsteret@users.noreply.github.com>
2023-10-19 11:08:38 +0200

    Add files via upload

---
+++ opgave 11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+
+double m;
+double n;
+
+double side1;
+double side2; 
+double h;
+
+
+printf("skriv m og n:\n"); 
+scanf("%lf %lf", &m, &n);
+
+side1 = m*m-n*n;
+side2 = 2*m*n;
+
+h = m*m+n*n;
+
+printf("de tre sider er %lf/n %lf/n %lf/n", side1, side2, h);
+
+return 0;
+
+}
